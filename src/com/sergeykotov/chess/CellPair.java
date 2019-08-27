36008254d2b552962d821c8ddab10ffca0919c65
Sergey Kotov <sergey1kotov@protonmail.com>
2019-08-28 00:20:58 +0200

    remove CellPair Pool: performance test has shown it is a counterproductive approach

--- src/com/sergeykotov/chess/CellPair.java
+++ src/com/sergeykotov/chess/CellPair.java
@@ -1,27 +1,8 @@
 package com.sergeykotov.chess;
 
-import java.util.HashSet;
 import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
 
 public final class CellPair {
-    private static final Set<CellPair> cellPairPool = new HashSet<>();
-
-    public static CellPair create(Cell cell1, Cell cell2) {
-        Optional<CellPair> p = cellPairPool.stream()
-                .filter(c -> c.cell1.equals(cell1) && c.cell2.equals(cell2))
-                .findFirst();
-        if (p.isPresent()) {
-            return p.get();
-        }
-        int dx = Math.abs(cell1.getX() - cell2.getX());
-        int dy = Math.abs(cell1.getY() - cell2.getY());
-        CellPair cellPair = new CellPair(cell1, cell2, dx, dy);
-        cellPairPool.add(cellPair);
-        return cellPair;
-    }
-
     public final Cell cell1;
     public final Cell cell2;
     public final int dx;
@@ -46,5 +27,11 @@ public final class CellPair {
     @Override
     public int hashCode() {
         return Objects.hash(cell1, cell2);
+    }
+
+    public static CellPair create(Cell cell1, Cell cell2) {
+        int dx = Math.abs(cell1.getX() - cell2.getX());
+        int dy = Math.abs(cell1.getY() - cell2.getY());
+        return new CellPair(cell1, cell2, dx, dy);
     }
 }
