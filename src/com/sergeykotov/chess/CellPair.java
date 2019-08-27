db884d7520001110a661034881ad089e57f02445
Sergey Kotov <sergey1kotov@protonmail.com>
2019-08-28 00:36:20 +0200

    refactor CellPair

--- src/com/sergeykotov/chess/CellPair.java
+++ src/com/sergeykotov/chess/CellPair.java
@@ -1,7 +1,5 @@
 package com.sergeykotov.chess;
 
-import java.util.Objects;
-
 public final class CellPair {
     public final Cell cell1;
     public final Cell cell2;
@@ -13,20 +11,6 @@ public final class CellPair {
         this.cell2 = cell2;
         this.dx = dx;
         this.dy = dy;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CellPair cellPair = (CellPair) o;
-        return Objects.equals(cell1, cellPair.cell1) &&
-                Objects.equals(cell2, cellPair.cell2);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(cell1, cell2);
     }
 
     public static CellPair create(Cell cell1, Cell cell2) {
