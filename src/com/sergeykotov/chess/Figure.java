360169f65bf6c5fa70e88f648a9070a880179257
Sergey Kotov <sergey1kotov@protonmail.com>
2020-05-01 10:48:50 +0200

    fix Figure.ROOK validatable interface implementation

--- src/com/sergeykotov/chess/Figure.java
+++ src/com/sergeykotov/chess/Figure.java
@@ -3,7 +3,7 @@ package com.sergeykotov.chess;
 public enum Figure {
     QUEEN(c -> c.cell1.getX() != c.cell2.getX() && c.cell1.getY() != c.cell2.getY() && c.dx != c.dy),
     KING(c -> c.dx > 1 && c.dy > 1),
-    ROOK(c -> c.cell1.getX() != c.cell2.getX() && c.cell2.getY() != c.cell2.getY()),
+    ROOK(c -> c.cell1.getX() != c.cell2.getX() && c.cell1.getY() != c.cell2.getY()),
     BISHOP(c -> c.dx != c.dy),
     KNIGHT(c -> (c.dx != 1 || c.dy != 2) && (c.dx != 2 || c.dy != 1));
 
